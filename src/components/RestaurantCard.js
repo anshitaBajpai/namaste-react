f3b265a6c95d8b869c7abf83cf4815485a790271
anshitaBajpai <anshitapsit@gmail.com>
2023-04-14 19:15:59 +0530

    episode-05

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,30 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+  } = resData?.data;
+
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+      ></img>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h5>{deliveryTime} mins</h5>
+    </div>
+  );
+};
+
+export default RestaurantCard;
