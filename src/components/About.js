77570aeeb2eb4fdf3dfbf2a3ded6c1b4b1bbbeac
anshitaBajpai <anshitapsit@gmail.com>
2023-06-20 23:46:00 +0530

    Routing

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="About">
+      <h1>About</h1>
+      <h2>Work in progress</h2>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Body.js
+++ src/components/Body.js
@@ -8,10 +8,6 @@ const Body = () => {
 
   const [searchText, setSearchText] = useState("");
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
